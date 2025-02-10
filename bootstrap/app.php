8f601ace03cad7b452b58ef8b23209a4ad894506
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-10 21:09:50 +0700

    11. Konsfigurasi midlleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,20 +4,19 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__)) // Menyusun dan mengonfigurasi aplikasi dengan basePath di direktori induk
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',        // Menentukan file routing untuk aplikasi web
-        commands: __DIR__.'/../routes/console.php', // Menentukan file routing untuk perintah (console commands)
-        health: '/up',                            // Menentukan endpoint untuk memeriksa status kesehatan aplikasi
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) { // Menambahkan middleware kustom ke aplikasi
-        return [
-            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class, // Alias 'role' digunakan untuk middleware yang memeriksa peran pengguna
-            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class, // Alias 'permission' untuk middleware izin pengguna
-            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class, // Alias 'role_or_permission' untuk middleware yang memeriksa peran atau izin
-        ];
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) { // Fungsi untuk menangani pengecualian (error)
-        // Biasanya digunakan untuk menyesuaikan cara pengecualian diproses
-    })
-    ->create(); // Membuat dan mengembalikan instance aplikasi yang telah dikonfigurasi
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
