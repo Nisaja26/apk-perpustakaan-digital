d6175e0d2103481373ea8120e71c71a7f7474840
nisaja26 <nissaikarachmawati@gmail.com>
2025-02-10 21:07:57 +0700

    10. Role Permmissions (konsfigurasi model user)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,14 +11,13 @@ return Application::configure(basePath: dirname(__DIR__)) // Menyusun dan mengon
         health: '/up',                            // Menentukan endpoint untuk memeriksa status kesehatan aplikasi
     )
     ->withMiddleware(function (Middleware $middleware) { // Menambahkan middleware kustom ke aplikasi
-        $middleware->alias([ // Menentukan alias untuk middleware
+        return [
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class, // Alias 'role' digunakan untuk middleware yang memeriksa peran pengguna
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class, // Alias 'permission' untuk middleware izin pengguna
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class, // Alias 'role_or_permission' untuk middleware yang memeriksa peran atau izin
-        ]);
+        ];
     })
     ->withExceptions(function (Exceptions $exceptions) { // Fungsi untuk menangani pengecualian (error)
         // Biasanya digunakan untuk menyesuaikan cara pengecualian diproses
-        //
     })
     ->create(); // Membuat dan mengembalikan instance aplikasi yang telah dikonfigurasi
