6203dd66ca24797cc14310d5271b921cad70b9d8
nisaja26 <nissaikarachmawati@gmail.com>
2025-03-03 09:58:14 +0700

    64. Migration Books dan Collections

--- database/migrations/2025_03_03_025155_create_collections_table.php
+++ database/migrations/2025_03_03_025155_create_collections_table.php
@@ -4,24 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCollectionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('location');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('collections');
     }
-};
+}
